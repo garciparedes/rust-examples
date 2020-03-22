449adde2add89080ab84bd9ae6023418c8bfd399
Sergio Garcia Prado <sergio@garciparedes.me>
2020-03-22 13:09:04 +0100

    * Included fifth chapter exercise of the Rust book.

---
+++ book/rectangles/src/main.rs
@@ -0,0 +1,39 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        other.width < self.width && other.height < self.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle { width: 30, height: 50 };
+
+    println!(
+        "The area of the rectangle \"{:?}\" is \"{}\" square pixels.",
+        rect1,
+        rect1.area(),
+    );
+
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let square = Rectangle::square(4);
+
+    println!("{:?}", square)
+}
